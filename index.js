4d648847098027f4ae19bfc56c581d9c785d9bcb
johneckert <johnteckert@gmail.com>
2018-02-01 15:36:09 -0500

    done

--- index.js
+++ index.js
@@ -3,22 +3,28 @@ function getFirstSelector(selector) {
 }
 
 function nestedTarget() {
-  var element = document.getElementById('nested');
-  return element.querySelector('.target');
+  return document.querySelector("#nested .target");
 }
 
 function increaseRankBy(n) {
-  var n = parseInt(n);
-  var lis = document.getElementById('app').querySelectorAll('ul.ranked-list li')
-  for (var i = 0; i < lis.length; i++) {
-    lis[i].innerHTML = (parseInt(lis[i].innerHTML) + n).toString();
+  const lis = document.getElementsByClassName("ranked-list");
+  for (let i = 0; i < lis.length; i++) {
+    let rank = parseInt(lis[i].innerHTML);
+    rank + n;
+    lis[i].innerHTML = rank;
   }
 }
 
 function deepestChild() {
-  let divs = document.querySelector('#grand-node');
-  while (divs.querySelector('div') != null) {
-    divs = divs.querySelector('div')
-  }
-  return divs;
+  let deepest = function(currentNode) {
+    if (currentNode.children.length == 0) {
+      return currentNode;
+    }
+
+    return deepest(currentNode.children[0]);
+  };
+
+  const startNode = document.querySelector("#grand-node");
+  let bottom = deepest(startNode);
+  return bottom;
 }
